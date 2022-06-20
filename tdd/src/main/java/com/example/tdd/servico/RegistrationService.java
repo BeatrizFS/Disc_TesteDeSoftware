759c81b373ccd57be45d04ffd00f8fe36e978634
BeatrizFS <beatrizsilva.sms@gmail.com>
2022-06-20 10:32:51 -0300

    teste4-TelefoneValidador reprova

---
+++ tdd/src/main/java/com/example/tdd/servico/RegistrationService.java
@@ -0,0 +1,9 @@
+package com.example.tdd.servico;
+
+
+
+public class RegistrationService {
+
+
+
+}
---
+++ tdd/src/test/java/com/example/tdd/utilis/TelefoneValidatorTest.java
@@ -0,0 +1,19 @@
+package com.example.tdd.utilis;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+public class TelefoneValidatorTest {
+
+    private TelefoneValidador underTest;
+
+    @BeforeEach
+    void setUp(){
+        underTest = new TelefoneValidador();
+    }
+
+    @Test
+    void itShouldValidadorTelefoneNum(){
+
+    }
+}
