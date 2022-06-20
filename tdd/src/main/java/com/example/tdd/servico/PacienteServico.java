eda89fef37366d0b8e8496f48594e2ca0075a069
BeatrizFS <beatrizsilva.sms@gmail.com>
2022-06-19 21:46:22 -0300

    teste1 passa

---
+++ tdd/src/main/java/com/example/tdd/servico/PacienteServico.java
@@ -0,0 +1,11 @@
+package com.example.tdd.servico;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PacienteServico {
+
+    public int diasCalculadorComDatabase(String nome) {
+        return 10;
+    }
+}
--- tdd/src/test/java/com/example/tdd/PacienteServicoTest.java
+++ tdd/src/test/java/com/example/tdd/PacienteServicoTest.java
@@ -1,13 +1,25 @@
 package com.example.tdd;
 
+import com.example.tdd.servico.PacienteServico;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.context.annotation.Bean;
 
 @RunWith(SpringRunner.class)
 public class PacienteServicoTest {
+
+    @TestConfiguration
+    static class PacienteServiceTestConfiguration{
+        @Bean
+        public PacienteServico pacienteServico(){
+            return new PacienteServico();
+        }
+
+    }
 
     @Autowired
     PacienteServico pacienteServico;
@@ -17,7 +29,6 @@ public class PacienteServicoTest {
     public void pacienteTestServicoDiasCalulador(){
         String nome = "Oliver";
         int dias = pacienteServico.diasCalculadorComDatabase(nome);
-
 
         //int i = 10;
         Assertions.assertEquals(dias, 10);
